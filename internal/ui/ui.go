a2c833aed132d2304dfa8ecd6775c2ec6a3893f7
Joshua Gilman <joshuagilman@gmail.com>
2020-04-24 10:31:03 -0700

    Adds documentation to the ui package

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,3 +1,4 @@
+// The ui package contains functions for retrieving information from an end-user via the CLI.
 package ui
 
 import (
@@ -6,10 +7,13 @@ import (
 )
 
 //go:generate moq -out ../../internal/mocks/prompterinterface.go -pkg mocks . Prompter
+// Prompter is used for testing purposes.
 type Prompter interface {
 	Run() (string, error)
 }
 
+// NewPrompt returns a promptui.Prompt which has its prompt message configured to the given message and adds an
+// additional character mask if hidden is set to true.
 func NewPrompt(message string, hidden bool) Prompter {
 	if !hidden {
 		return &promptui.Prompt{
@@ -23,6 +27,8 @@ func NewPrompt(message string, hidden bool) Prompter {
 	}
 }
 
+// NewSelectPrompt returns a promptui.SelectPrompt with its prompt message configured to the given message and the
+// available options for the user to select configured to the given string slice.
 func NewSelectPrompt(message string, options []string) *promptui.Select {
 	return &promptui.Select{
 		Label: message,
@@ -30,6 +36,9 @@ func NewSelectPrompt(message string, options []string) *promptui.Select {
 	}
 }
 
+// GetAuthDetails retrieves the authentication details from the given authentication type and proceeds to prompt the
+// user to provide input for each of the retrieved details. It returns the detail map configured with the input data
+// from the end-user.
 func GetAuthDetails(a auth.Auth, prompterFactory func(message string, hidden bool) Prompter) (map[string]*auth.Detail, error) {
 	details := a.AuthDetails()
 	for _, detail := range details {
